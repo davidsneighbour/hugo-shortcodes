493855cb68704ca316b2abce40e961afdcde8906
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-06-21 22:09:47 +0700

    chore: fix configuration

--- youtube/bin/release-hook-postrelease.sh
+++ youtube/bin/release-hook-postrelease.sh
@@ -3,4 +3,4 @@
 git push --follow-tags origin main
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-printf "${RED}Don't forget to update shortcodes/all${NC}\n"
+printf "${RED}Don't forget to update shortcodes/all!${NC}\n"
